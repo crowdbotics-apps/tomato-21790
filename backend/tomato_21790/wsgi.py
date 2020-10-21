390f81780ff928d18bb2b3f490471a80073a240c
Crowdbotics <team@crowdbotics.com>
2020-10-21 15:08:22 +0000

    Update models

--- backend/tomato_21790/wsgi.py
+++ backend/tomato_21790/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tomato_21790.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tomato_21790.settings")
 
 application = get_wsgi_application()
